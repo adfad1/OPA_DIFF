701f21aeddfbed2bd16e75e0d6d24466f7fae9f9
zyang <1187192530@qq.com>
2018-04-05 11:29:59 +0800

    path.append->path.insert

--- path.py
+++ path.py
@@ -3,7 +3,7 @@ import getpass
 
 user = getpass.getuser()
 
-sys.path.append('/home/%s/OPA_DIFF/Environments'%user)
-sys.path.append('/home/%s/OPA_DIFF/baselines'%user)
+sys.path.insert(1,'/home/%s/OPA_DIFF/Environments'%user)
+sys.path.insert(1,'/home/%s/OPA_DIFF/baselines'%user)
 
 print(sys.path)
