cdefeae87ba1944a524797f14f575ab0be05b84a
zyang <1187192530@qq.com>
2018-04-05 19:34:17 +0800

    change init.py

--- path.py
+++ path.py
@@ -5,10 +5,6 @@ user = getpass.getuser()
 
 sys.path.insert(1,'/home/%s/OPA_DIFF/Environments'%user)
 sys.path.insert(1,'/home/%s/OPA_DIFF/baselines'%user)
-sys.path.insert(2,'/home/%s/OPA_DIFF/baselines/baselines/common'%user)
-sys.path.insert(2,'/home/%s/OPA_DIFF/baselines/baselines/bench'%user)
-sys.path.insert(2,'/home/%s/OPA_DIFF/baselines/baselines/ppo1'%user)
-
 
 
 print(sys.path)
